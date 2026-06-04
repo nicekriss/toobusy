0f96565631c052738999344c6fdc5148305d8570
nicekriss <130124701+nicekriss@users.noreply.github.com>
2026-06-04 10:08:51 +0900

    Add Ideogram layout builder node

--- __init__.py
+++ __init__.py
@@ -5,8 +5,23 @@ This repository is intended to be cloned into ComfyUI's `custom_nodes` folder
 so this top-level `__init__.py` must expose node mappings.
 """
 
-from .hf_model_auto_loader import (  # noqa: F401
-    NODE_CLASS_MAPPINGS,
-    NODE_DISPLAY_NAME_MAPPINGS,
-    WEB_DIRECTORY,
+from .hf_model_auto_loader import (
+    NODE_CLASS_MAPPINGS as HF_NODE_CLASS_MAPPINGS,
+    NODE_DISPLAY_NAME_MAPPINGS as HF_NODE_DISPLAY_NAME_MAPPINGS,
 )
+from .ideogram_layout_builder import (
+    NODE_CLASS_MAPPINGS as IDEOGRAM_NODE_CLASS_MAPPINGS,
+    NODE_DISPLAY_NAME_MAPPINGS as IDEOGRAM_NODE_DISPLAY_NAME_MAPPINGS,
+)
+
+NODE_CLASS_MAPPINGS = {
+    **HF_NODE_CLASS_MAPPINGS,
+    **IDEOGRAM_NODE_CLASS_MAPPINGS,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    **HF_NODE_DISPLAY_NAME_MAPPINGS,
+    **IDEOGRAM_NODE_DISPLAY_NAME_MAPPINGS,
+}
+
+WEB_DIRECTORY = "./js"
