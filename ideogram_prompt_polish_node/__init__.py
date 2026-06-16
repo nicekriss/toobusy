f0770b0252115b30366689003be5938f7c27e525
nicekriss <130124701+nicekriss@users.noreply.github.com>
2026-06-16 18:37:24 +0900

    Make prompt polish layout output compact

--- ideogram_prompt_polish_node/__init__.py
+++ ideogram_prompt_polish_node/__init__.py
@@ -5,7 +5,13 @@ from ..keyframe_maker_node import keyframe_maker as _keyframe_maker
 _NO_SEGMENT = "_split" + "_mixed_text_elements"
 setattr(_prompt_polish, _NO_SEGMENT, lambda payload: payload)
 
+_ORIGINAL_BUILD_PROMPT = _prompt_polish._build_prompt
 _ORIGINAL_GENERATE_TEXT = _keyframe_maker._generate_text
+
+
+def _build_prompt_compact(*args, **kwargs):
+    prompt = _ORIGINAL_BUILD_PROMPT(*args, **kwargs)
+    return prompt + "\n\nCompact JSON target: 5-8 major layout regions, one box per grouped label or visual block, skip minor details."
 
 
 def _generate_text_capped(*args, **kwargs):
@@ -14,6 +20,7 @@ def _generate_text_capped(*args, **kwargs):
     return _ORIGINAL_GENERATE_TEXT(*args, **kwargs)
 
 
+_prompt_polish._build_prompt = _build_prompt_compact
 _keyframe_maker._generate_text = _generate_text_capped
 
 NODE_CLASS_MAPPINGS = _prompt_polish.NODE_CLASS_MAPPINGS
